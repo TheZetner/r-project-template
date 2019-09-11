bb0e171d6e7617a2f0d91c3df58d0d62e3a59f28
Adrian Zetner <adrian.zetner@canada.ca>
2019-09-11 14:02:56 -0500

    Minimal version edits

---
+++ 01_read-and-tidy.R
@@ -0,0 +1,5 @@
+# Read and tidy raw data
+# DATE
+# YOUR NAME HERE
+
+# Use this file for reading in the raw data. Save interim outputs (eg. cleaned data) to output folder
---
+++ 02_plot.R
@@ -0,0 +1,5 @@
+# Create Visualizations
+# DATE
+# YOUR NAME HERE
+
+# Use this file for generating plots etc... from your tidied data
